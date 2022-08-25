da6e6d19ea606f4128f06dc95518d5466168c126
Thomas <tpyke117@gmail.com>
2022-08-25 20:15:29 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Thomas Pyke
+ *         thomaspyke@unomaha.edu
+ * Date: 2022/08/23
+ *
+ * Hack 1 Print Name File
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Thomas Pyke\n");
+
+  return 0;
+}
